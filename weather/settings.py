48621ed691439c8c2d46d22ffa79c718233ec5b3
Vinraj1 <160110182+Vinraj1@users.noreply.github.com>
2025-11-06 07:59:20 +0530

    Update Django settings with static files configuration

--- weather/settings.py
+++ weather/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -126,4 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
 
