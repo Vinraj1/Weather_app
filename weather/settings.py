0d4b744b5b11a3a3d9e5b887322668783a7e83ba
Vinraj1 <160110182+Vinraj1@users.noreply.github.com>
2026-03-05 15:21:09 +0000

    '

--- weather/settings.py
+++ weather/settings.py
@@ -64,7 +64,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
 {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [BASE_DIR / 'weatherapp' / 'templates'/ 'index.html'],
+    'DIRS': [BASE_DIR / 'weatherapp' / 'templates'],
     'APP_DIRS': True,
     'OPTIONS': {
         'context_processors': [
