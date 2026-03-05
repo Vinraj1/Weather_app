ec4ef918bf2d100827336b6d67a9a1d4da7ad0c3
Vinraj1 <160110182+Vinraj1@users.noreply.github.com>
2026-03-05 10:33:42 +0530

    Update settings.py

--- weather/settings.py
+++ weather/settings.py
@@ -64,7 +64,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'weatherapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
